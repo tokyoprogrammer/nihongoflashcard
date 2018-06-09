21888c6887eae2ae95e94f39a8b93f0c86d27428
Yoseob Kim <yo1226@gmail.com>
2018-06-09 06:31:22 +0000

    Select Level Page modified

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -22,8 +22,13 @@ export default class MainPage extends React.Component {
   render() {
     return (
       <Page renderToolbar={this.renderToolbar}>
+        <img style={{width: '100%'}} src='img/konnichiwa.jpg' />
         <p style={{textAlign: 'center'}}>
-          <Button onClick={this.pushPage.bind(this, 5)}>N5</Button>
+          <Button style={{margin: '6px'}} modifier='large' onClick={this.pushPage.bind(this, 5)}>N5</Button>
+          <Button style={{margin: '6px'}} modifier='large' onClick={this.pushPage.bind(this, 4)}>N4</Button>
+          <Button style={{margin: '6px'}} modifier='large' onClick={this.pushPage.bind(this, 3)}>N3</Button>
+          <Button style={{margin: '6px'}} modifier='large' onClick={this.pushPage.bind(this, 2)}>N2</Button>
+          <Button style={{margin: '6px'}} modifier='large' onClick={this.pushPage.bind(this, 1)}>N1</Button>
         </p>
       </Page>
     );
